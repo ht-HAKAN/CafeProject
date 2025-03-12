d661d19433c0cb17ffb37c13226475791bf49d2c
Hakan Tuna <ht.tunahakan@gmail.com>
2025-03-12 06:41:16 +0300

    menu update commit

--- CafeProject/src/application/Form1Controller.java
+++ CafeProject/src/application/Form1Controller.java
@@ -1,5 +1,28 @@
 package application;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class Form1Controller {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField KullaniciAdiGiris;
+
+    @FXML
+    private TextField SifreGiris;
+
+    @FXML
+    void initialize() {
+        assert KullaniciAdiGiris != null : "fx:id=\"KullaniciAdiGiris\" was not injected: check your FXML file 'Form.fxml'.";
+        assert SifreGiris != null : "fx:id=\"SifreGiris\" was not injected: check your FXML file 'Form.fxml'.";
+
+    }
+
 }
