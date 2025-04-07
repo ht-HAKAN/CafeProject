c3f0e4296bfa62a2faeadc89147e47f7b6444df5
Hakan Tuna <ht.tunahakan@gmail.com>
2025-04-07 12:37:39 +0300

    admin panel mysql connection

--- CafeProject/src/application/Form1Controller.java
+++ CafeProject/src/application/Form1Controller.java
@@ -7,6 +7,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Form1Controller {
 
     @FXML
@@ -23,7 +27,6 @@ public class Form1Controller {
 
     @FXML
     void initialize() {
-        // Giriş Yap butonuna tıklama işlemi
         GirisYapButton.setOnAction(event -> {
             String kullaniciAdi = KullaniciAdiGiris.getText();
             String sifre = SifreGiris.getText();
@@ -31,8 +34,7 @@ public class Form1Controller {
             if (kullaniciAdi.isEmpty() || sifre.isEmpty()) {
                 showAlert("Hata", "Kullanıcı adı veya şifre boş olamaz!", AlertType.ERROR);
             } else {
-                //  örnek
-                if (kullaniciAdi.equals("admin") && sifre.equals("1234")) {
+                if (girisKontrol(kullaniciAdi, sifre)) {
                     showAlert("Başarılı", "Giriş başarılı!", AlertType.INFORMATION);
                 } else {
                     showAlert("Hata", "Kullanıcı adı veya şifre hatalı!", AlertType.ERROR);
@@ -40,13 +42,29 @@ public class Form1Controller {
             }
         });
 
-        // Misafir Olarak Devam Et butonuna tıklama işlemi
         GirisYapButton1.setOnAction(event -> {
             showAlert("Misafir Girişi", "Misafir olarak devam ediyorsunuz.", AlertType.INFORMATION);
         });
     }
 
-    // Hata veya bilgi mesajlarının gösterilmesi
+    private boolean girisKontrol(String kullaniciAdi, String sifre) {
+        try {
+            Connection conn = MySQLConnection.connect();
+            // SQL SORGULAMA 
+            String sql = "SELECT * FROM adminler WHERE kullanici_adi = ? AND sifre = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, kullaniciAdi);
+            stmt.setString(2, sifre);
+
+            ResultSet rs = stmt.executeQuery();
+            return rs.next(); // eşleşme varsa true döner
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     private void showAlert(String title, String message, AlertType alertType) {
         Alert alert = new Alert(alertType);
         alert.setTitle(title);
